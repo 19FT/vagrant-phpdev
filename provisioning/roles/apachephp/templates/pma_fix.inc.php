5e096a5b3527a4a8cdc56a5b2c7efa4f492cd4ce
Rob Allen <rob@akrabat.com>
2014-10-01 11:51:39 +0100

    Fix PHPMyAdmin configuration.

---
+++ provisioning/roles/apachephp/templates/pma_fix.inc.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Fix for PHPMyadmin config that's installed by Ubuntu
+ */
+
+$i = 1;
+
+/* Optional: User for advanced features */
+$cfg['Servers'][$i]['controluser'] = 'root';
+$cfg['Servers'][$i]['controlpass'] = '{{mysql_root_password}}';
+
+/* Optional: Advanced phpMyAdmin features */
+$cfg['Servers'][$i]['pmadb'] = $dbname;
+$cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+$cfg['Servers'][$i]['relation'] = 'pma__relation';
+$cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+$cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+$cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+$cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+$cfg['Servers'][$i]['history'] = 'pma__history';
+$cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+$cfg['Servers'][$i]['designer_coords'] = 'pma__designer_coords';
+$cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+$cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+$cfg['Servers'][$i]['recent'] = 'pma__recent';
+
