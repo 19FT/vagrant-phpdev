ff838a5fa4710dc4ea00347ebd2c652568dcc6d7
Rob Allen <rob@akrabat.com>
2015-06-01 12:17:36 +0100

    PHP 5.6

--- vm-provisioning/init.sh
+++ vm-provisioning/init.sh
@@ -2,16 +2,18 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
+
 if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    echo "Add APT repositories"
+    echo "Add repositories"
     apt-get install -qq software-properties-common &> /dev/null || exit 1
     apt-add-repository ppa:ansible/ansible &> /dev/null || exit 1
-    apt-add-repository ppa:ondrej/php5 &> /dev/null || exit 1
+    apt-add-repository ppa:ondrej/php5-5.6 &> /dev/null || exit 1
 
     apt-get update -qq
 
-    echo "Installing ansible"
+    echo "Installing Ansible"
     apt-get install -qq ansible &> /dev/null || exit 1
 
     echo "Ansible installed"
